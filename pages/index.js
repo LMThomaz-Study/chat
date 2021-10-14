b291263e71f1a90d420278ce17c5826d8604c56d
Leonardo Thomaz <leonardo.thomaz@hotmail.com>
2021-10-14 05:02:11 -0300

    Init project

---
+++ pages/index.js
@@ -0,0 +1,16 @@
+import Head from 'next/head';
+import Image from 'next/image';
+import styles from '../styles/Home.module.css';
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Chat 2.0</title>
+        <link rel='icon' href='/favicon.ico' />
+      </Head>
+
+      <h1>Lets build Chat 2.0</h1>
+    </div>
+  );
+}
