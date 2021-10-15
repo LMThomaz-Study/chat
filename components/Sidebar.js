1345811cc55ad6a632efb103c15aad56af6fdfcf
Leonardo Thomaz <leonardo.thomaz@hotmail.com>
2021-10-15 13:02:07 -0300

    Building the Sidebar Component (2/3)

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -2,9 +2,22 @@ import { Avatar, IconButton, Button } from '@material-ui/core';
 import ChatIcon from '@material-ui/icons/Chat';
 import MoreVertIcon from '@material-ui/icons/MoreVert';
 import SearchIcon from '@material-ui/icons/Search';
+import * as EmailValidator from 'email-validator';
 import styled from 'styled-components';
 
 export function Sidebar() {
+  const createChat = () => {
+    const input = prompt(
+      'Please enter an email address hot the user you wish to chat with',
+    );
+
+    if (!input) return null;
+
+    if (EmailValidator.validate(input)) {
+      // We need to add the chat into the DB 'chats' collection
+    }
+  };
+
   return (
     <Container>
       <Header>
@@ -25,7 +38,7 @@ export function Sidebar() {
         <SearchInput placeholder='Search in chats' />
       </Search>
 
-      <SidebarButton>Start a new chat</SidebarButton>
+      <SidebarButton onClick={createChat}>Start a new chat</SidebarButton>
 
       {/* List of Chats */}
     </Container>
